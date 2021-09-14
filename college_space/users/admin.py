ae6d0f4e65be24b89614acf99ade025baeaa9f86
Upendra Kumar <uk06704@gmail.com>
2021-09-14 20:01:44 +0530

    intial project: contains users model and basic project settings

---
+++ college_space/users/admin.py
@@ -0,0 +1,29 @@
+from .models import User
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from . import forms 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    form = forms.UserChangeForm
+    add_form = forms.UserCreationForm
+
+    list_display = ['email', 'first_name', 'last_name', 'semester']
+    list_filter = ['email', 'semester', 'first_name', 'is_admin']
+
+    fieldsets = [
+        (None, {'fields':('email', 'password')}),
+        ('Personal Info', {'fields':('first_name', 'last_name', 'semester')}),
+        ('Permissions', {'fields':('is_admin',)})
+    ]
+
+    add_fieldsets = (
+        (None, {'fields':('email', 'password1', 'password2', 'first_name', 'last_name', 'semester')}),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
