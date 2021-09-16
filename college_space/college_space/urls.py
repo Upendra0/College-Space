2aafe964f8dd759e36e3b43f866d1fd773b6c90e
Upendra Kumar <uk06704@gmail.com>
2021-09-16 10:27:58 +0530

    created .gitignore file to ignore env and .vscode folder

--- college_space/college_space/urls.py
+++ college_space/college_space/urls.py
@@ -15,4 +15,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('resources.urls')),
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
 ]
--- college_space/users/views.py
+++ college_space/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from . import forms
 
 # Create your views here.
+
+def register(request):
+    if request.method=='POST':
+        form = forms.UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='home')
+    form = forms.UserCreationForm()
+    return render(request=request, template_name='users/register.html', context={'form':form})
+
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
