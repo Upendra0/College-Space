a97bc1465fcc6523df8cc05786ba523f82c03d23
Atulya Kumar <67427578+atulya-karn@users.noreply.github.com>
2021-09-22 14:39:44 +0530

    fix:model

--- college_space/resources/admin.py
+++ college_space/resources/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Subject, Resource
+from .models import Subject, Resource, Syllabus
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display= ['name', 'department', 'semester', 'credit']
+    list_filter= ['department', 'semester', 'credit']
+
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'resource_type', 'get_semester', 'get_department','author', 'link']
+    list_filter= ['author']
+
+class SyllabusAdmin(admin.ModelAdmin):
+    list_display = ['department', 'semester', 'download_link']
+    list_filter= ['department', 'semester']
 
 # Register your models here.
-admin.site.register(Subject)
-admin.site.register(Resource)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Resource, ResourceAdmin)
+admin.site.register(Syllabus, SyllabusAdmin)
--- college_space/resources/models.py
+++ college_space/resources/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.core.validators import  MinValueValidator, MaxValueValidator
+from django.contrib import admin
+
+department_type_choices = (
+    ('cse', 'Computer Science & engineering'),
+    ('ce', 'Civil engineering'),
+    ('ee', 'Electrical engineering'),
+    ('ece', 'Electronics and communication engineering'),
+)
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=50)
-    syllabus = models.CharField(max_length=500)
-    semester = models.SmallIntegerField()
+    department = models.CharField( max_length=255, choices=department_type_choices)
+    semester = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)])
     credit = models.SmallIntegerField()
-
+    
     def __str__(self) -> str:
         return self.name
 
@@ -22,4 +32,27 @@ class Resource(models.Model):
     author = models.CharField(max_length=500)
     link = models.CharField(max_length=500)
     description = models.TextField()
-    rating = models.SmallIntegerField()
+    rating = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    def __str__(self) -> str:
+        return self.subject.name + ' (' + self.resource_type + ')'
+
+    @admin.display(description="Semester ")
+    def get_semester(self):
+        return self.subject.semester
+
+    @admin.display(description="Department")
+    def get_department(self):
+        return self.subject.department
+
+
+class Syllabus(models.Model):
+    department = models.CharField( max_length=255, choices=department_type_choices)
+    semester = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)])
+    download_link = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'syllabuse'
+
+    def __str__(self) -> str:
+        return self.department + '(sem-' + str(self.semester) + ')'
