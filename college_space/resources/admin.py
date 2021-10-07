b83c124627937f91c310d6f81e981837aad12d17
Upendra Kumar <uk06704@gmail.com>
2021-10-07 18:04:50 +0530

    provided the search and ordering to every model

--- college_space/resources/admin.py
+++ college_space/resources/admin.py
@@ -4,42 +4,60 @@ from . import models
 
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ['name']
+    search_fields = ['name']
 
 class SubjectAdmin(admin.ModelAdmin):
     list_display = [ 'code', 'name', 'credit']
     list_filter = ['credit']
+    ordering = ['name']
+    search_fields = ['code', 'name']
 
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name', 'subject']
     list_filter = ['subject']
+    ordering = ['name']
+    search_fields = ['name', 'subject__name']
 
 class TaughtAdmin(admin.ModelAdmin):
     list_display = ['subject', 'department', 'semester']
     list_filter = ['department', 'semester']
+    ordering = ['department']
+    search_fields = ['subject__name', 'department__name']
 
 class SyllabusAdmin(admin.ModelAdmin):
     list_display = ['department', 'semester', 'view_link']
     list_filter = ['department', 'semester']
+    ordering= ['department']
+    search_fields = ['department']
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ['topic', 'file', 'contributor', 'is_approved']
     list_filter = ['is_approved', 'contributor']
+    search_fields = ['topic__name', 'contributor__email']
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ['subject', 'name', 'view_link', 'contributor', 'is_approved']
     list_filter = ['is_approved', 'contributor']
+    ordering = ['name']
+    search_fields = ['name', 'subject__name', 'contributor__email']
 
 class WebTutorialAdmin(admin.ModelAdmin):
     list_display = ['subject', 'name', 'view_link', 'contributor', 'is_approved']
     list_filter = ['is_approved', 'contributor']
+    ordering = ['name']
+    search_fields = ['name', 'subject__name','contributor__email']
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ['subject', 'name', 'view_link', 'contributor', 'is_approved']
     list_filter = ['is_approved', 'contributor']
+    ordering = ['name']
+    search_fields = ['name', 'subject__name', 'contributor__email']
 
 class QuestionPaperAdmin(admin.ModelAdmin):
     list_display = ['subject', 'year', 'file', 'contributor', 'is_approved']
     list_filter = ['is_approved', 'year', 'contributor']
+    ordering = ['subject']
+    search_fields = ['subject__name', 'year', 'contributor__email']
 
 
 admin.site.register(models.Department, DepartmentAdmin)
