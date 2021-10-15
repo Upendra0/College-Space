162c820cbcf62fe4be64c53394806436dd40e71c
Upendra Kumar <uk06704@gmail.com>
2021-10-15 18:57:34 +0530

    Changed the dept_sem snippet form to show inline.

--- college_space/resources/forms.py
+++ college_space/resources/forms.py
@@ -6,5 +6,5 @@ class DepartmentSemesterForm(forms.Form):
     dept_choices = [ (ele.get('name'), ele.get('name')) for ele in dept_choices]
     sem_choices = [1,2,3,4,5,6,7,8]
     sem_choices = [ (ele, ele) for ele in sem_choices]
-    dept_name = forms.ChoiceField(choices=dept_choices)
-    semester = forms.ChoiceField(choices=sem_choices)
+    dept_name = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control','id':'dept_name', 'placeholder':'Choose Department'}), choices=dept_choices)
+    semester = forms.ChoiceField(widget=forms.Select(attrs={'class':'form-control','id':'semester', 'placeholder':'Choose Semester'}), choices=sem_choices)
