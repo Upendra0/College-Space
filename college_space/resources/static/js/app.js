2597de9c9b1b20f6d115ce3d4e621de40be43fe7
Jawed Akhtar <jk9709371@gmail.com>
2021-11-08 01:16:48 +0530

    style&fix:fix some heigth error and style

--- college_space/resources/static/js/app.js
+++ college_space/resources/static/js/app.js
@@ -1,13 +1,13 @@
 const sidepanel = document.querySelector("#mySidepanel");
 
 function openNav() {
-    sidepanel.style.width = "350px";
-  }
+  sidepanel.style.width = "350px";
+}
 
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    sidepanel.style.width = "0";
-  };
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  sidepanel.style.width = "0";
+};
 
 var loadFile = function (event) {
   var image = document.getElementById("output");
