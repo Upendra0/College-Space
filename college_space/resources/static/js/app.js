9fcc2c83b7510f060af5e247fe3f0b5f645d2e51
Jawed Akhtar <jk9709371@gmail.com>
2021-11-10 00:01:30 +0530

    style&fix

--- college_space/resources/static/js/app.js
+++ college_space/resources/static/js/app.js
@@ -9,7 +9,14 @@ function closeNav() {
   sidepanel.style.width = "0";
 };
 
-var loadFile = function (event) {
-  var image = document.getElementById("output");
+let loadFile = function (event) {
+  let image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
