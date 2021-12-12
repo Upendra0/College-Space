f367387ffdf3ac43f8b10ad5cd647a5ede6f3049
Upendra Kumar <uk06704@gmail.com>
2021-12-12 22:16:28 +0530

    added static root setting

--- college_space/college_space/settings.py
+++ college_space/college_space/settings.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import os
 import json
 
+#Json file storing credentials
 json_file = open('college_space/config.json')
 config = json.load(json_file)
 json_file.close()
@@ -80,8 +81,6 @@ WSGI_APPLICATION = 'college_space.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,8 +94,6 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -114,8 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE =  'Asia/Kolkata'
@@ -128,25 +123,24 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL= '/media/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
+#Crispy template css to autostyle django form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+#Redirect to authenticated user
 LOGIN_REDIRECT_URL='home'
 LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL='home'
 
+#Email address settings
 EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -154,12 +148,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config.get('EMAIL_HOST_PASSWORD')
 
-LOGOUT_REDIRECT_URL='home'
-
-
-
 # AWS S3 Setting
-
 AWS_ACCESS_KEY_ID = config.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = config.get("AWS_STORAGE_BUCKET_NAME")
