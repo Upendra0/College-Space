5fceb1ea439276328a2c045723f298fafc3700b7
Jawed Akhtar <jk9709371@gmail.com>
2021-12-16 12:17:09 +0530

    fix:mobileResponsive

--- college_space/resources/static/js/app.js
+++ college_space/resources/static/js/app.js
@@ -13,10 +13,3 @@ let loadFile = function (event) {
   let image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
-
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.elements.name.value = '';
-  e.target.elements.email.value = '';
-  e.target.elements.message.value = '';
-});
